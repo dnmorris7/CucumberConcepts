626db164671878a1394c76c215df3ac832c4915e
David <dnmorris7@gmail.com>
2022-08-25 21:31:20 -0400

    2022/08/25 21:35

--- src/test/java/StepDefinition/GoogleSearchSteps.java
+++ src/test/java/StepDefinition/GoogleSearchSteps.java
@@ -7,12 +7,14 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class GoogleSearchSteps {
+	final static String EDGE = "EDGE";
 	WebDriver driver = null;
 
 	//setup test.properties to point to your Selenium driver location.
@@ -51,8 +53,8 @@ public class GoogleSearchSteps {
 	public void browser_is_open() {
 		// Write code here that turns the phrase above into concrete actions
 		
-		System.setProperty("webdriver.chrome.driver", getDriverProperties());
-		driver = new ChromeDriver();
+		System.setProperty("webdriver.edge.driver", getDriverProperties(EDGE));
+		driver = new EdgeDriver();
 		driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
 		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
 		
